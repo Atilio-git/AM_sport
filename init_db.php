4cf5bf0fee42540a48f7aee4fecb219451719953
botonbatilio@gmail.com <botonbatilio@gmail.com>
2026-02-25 14:37:15 +0100

    init_db

---
+++ init_db.php
@@ -0,0 +1,124 @@
+<?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$host ='localhost';
+$DB_USER = 'root';
+$DB_PASS = 'root';
+
+try{
+    $pdo = new PDO("mysql:host=$host",$DB_USER,$DB_PASS);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    
+    $pdo->exec("CREATE DATABASE IF NOT EXISTS AM_SPORT ");
+
+    $pdo = new PDO("mysql:host=$host;dbname=AM_SPORT",$DB_USER,$DB_PASS);
+
+
+    $sql1="
+    CREATE TABLE clients (
+        id_client INT NOT NULL AUTO_INCREMENT,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        password_hash VARCHAR(255) NOT NULL,
+        nom_client VARCHAR(255) NOT NULL,
+        prenom VARCHAR(255) NOT NULL,
+        addresse_client VARCHAR(255) NOT NULL,
+        role ENUM('client', 'admin') NOT NULL DEFAULT 'client',
+        PRIMARY KEY (id_client)
+    ) ENGINE=InnoDB";
+
+    $sql2="
+        CREATE TABLE categorie (
+        id_categorie INT NOT NULL AUTO_INCREMENT,
+        nom_categorie VARCHAR(255) NOT NULL,
+        PRIMARY KEY (id_categorie)
+    ) ENGINE=InnoDB";
+
+    $sql3="
+        CREATE TABLE articles (
+        id_article INT NOT NULL AUTO_INCREMENT,
+        nom_article VARCHAR(255) NOT NULL,
+        id_categorie INT NOT NULL,
+        description TEXT NOT NULL,
+        PRIMARY KEY (id_article),
+        FOREIGN KEY (id_categorie)
+        REFERENCES categorie (id_categorie)
+    ) ENGINE=InnoDB";
+    $sql4="
+        CREATE TABLE infos_articles (
+        id_info INT NOT NULL AUTO_INCREMENT,
+        id_article INT NOT NULL,
+        taille VARCHAR(255) NOT NULL,
+        prix DECIMAL(10 , 2 ),
+        PRIMARY KEY (id_info),
+        FOREIGN KEY (id_article)
+        REFERENCES articles (id_article)
+    ) ENGINE=InnoDB";
+    $sql5="
+        CREATE TABLE commande (
+        id_client INT NOT NULL,
+        id_commande INT NOT NULL AUTO_INCREMENT,
+        date_commande TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        statut ENUM('en attente', 'payé', 'livré') DEFAULT 'en attente',
+        PRIMARY KEY (id_commande),
+        FOREIGN KEY (id_client)
+        REFERENCES clients (id_client)
+    ) ENGINE=InnoDB";
+    $sql6="
+        CREATE TABLE addresse (
+        id_addresse INT NOT NULL AUTO_INCREMENT,
+        id_client INT NOT NULL,
+        addresse_livraison VARCHAR(255),
+        pays VARCHAR(255) NOT NULL,
+        code_postal INT NOT NULL,
+        PRIMARY KEY (id_addresse),
+        FOREIGN KEY (id_client)
+        REFERENCES clients (id_client)
+    ) ENGINE=InnoDB";
+    $sql7="
+        CREATE TABLE panier (
+        id_panier INT NOT NULL AUTO_INCREMENT,
+        id_client INT NOT NULL,
+        id_article INT NOT NULL,
+        nom_article VARCHAR(255) NOT NULL,
+        prix_unitaire DECIMAL(10 , 2 ),
+        quantite INT NOT NULL,
+        prix_total DECIMAL(10 , 2 ),
+        PRIMARY KEY (id_panier),
+        FOREIGN KEY (id_client)
+        REFERENCES clients (id_client),
+        FOREIGN KEY (id_article)
+        REFERENCES articles (id_article)
+    ) ENGINE=InnoDB";
+    $sql8="
+        CREATE TABLE avis_clients (
+        id_avis INT NOT NULL AUTO_INCREMENT,
+        id_client INT NOT NULL,
+        nom_client VARCHAR(255) NOT NULL,
+        prenom VARCHAR(255) NOT NULL,
+        propos TEXT,
+        date_commentaire TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (id_avis),
+        FOREIGN KEY (id_client)
+        REFERENCES clients (id_client)
+    ) ENGINE=InnoDB";
+
+
+    $pdo->exec($sql1);
+    $pdo->exec($sql2);
+    $pdo->exec($sql3);
+    $pdo->exec($sql4);
+    $pdo->exec($sql5);
+    $pdo->exec($sql6);
+    $pdo->exec($sql7);
+    $pdo->exec($sql8);
+    
+
+}
+
+catch(PDOException $pe){
+    echo"ERREUR : ".$pe->getMessage();
+}
